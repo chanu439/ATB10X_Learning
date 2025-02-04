2e6396a56220e35e8f574a70d7831868c439c738
gumma <chanu439@gmail.com>
2025-02-04 23:06:18 +0530

    Test02

---
+++ src/ex_01_java_Basic/Lab003_Println_Print.java
@@ -0,0 +1,10 @@
+package ex_01_java_Basic;
+
+public class Lab003_Println_Print {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+        System.out.println("hello");
+        System.out.print("chanukya");
+        System.out.println("gummadidala");
+    }
+}
