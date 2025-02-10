dc7cf4e4274d76fa98afead142990f5a57361985
gumma <chanu439@gmail.com>
2025-02-10 19:39:31 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_01_java_Basic/Lab008_Common_Math.java
@@ -0,0 +1,12 @@
+package ex_01_java_Basic;
+
+public class Lab008_Common_Math {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(5-2);
+        System.out.println(4*3);
+        System.out.println(12/5);
+        /*println cannot give output for decimal values
+        it will print only whole integers */
+    }
+}
---
+++ src/ex_01_java_Basic/Lab009_Common_Math_exception.java
@@ -0,0 +1,7 @@
+package ex_01_java_Basic;
+
+public class Lab009_Common_Math_exception {
+    public static void main(String[] args) {
+        System.out.println(10/0);
+    }
+}
