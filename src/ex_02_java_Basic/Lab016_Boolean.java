e1bd00435c24ac684dee56f3833d5ff318341962
gumma <chanu439@gmail.com>
2025-02-11 12:09:24 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_02_java_Basic/Lab016_Boolean.java
@@ -0,0 +1,9 @@
+package ex_02_java_Basic;
+
+public class Lab016_Boolean {
+    public static void main(String[] args) {
+        boolean is_world_revolves=true;
+        boolean is_earth_rotates1 = false;
+        boolean this_is_a_very_long_variable_$$ = true;
+    }
+}
